bug fix: logic for resolution check was wrong, and flipping the < sign exposed an issue with including nodata values (instead of using a masked array)

ausseabed/mbesgc/lib/mbesgridcheck.py
@@ -599,18 +599,17 @@ class ResolutionCheck(GridCheck):
         )
 
         fds = np.ma.masked_where(np.ma.getmask(depth), fds)
-        fds.fill_value = -9999.0
-        fds = fds.filled()
 
         self.grid_resolution = ifd.geotransform[1]
         # easier to calc a feature size from a single grid resolution and the
         # FDS multiplier than to rescale the whole fds array
         feature_size = self.grid_resolution / self._fds_multiplier
 
-        # failed pixels is where the calculated feature detection size is
-        # larger than the feature size
-        # failed_resolution = fds > feature_size
-        failed_resolution = np.where(fds > feature_size, True, False)
+        # The idea of the standard here is that the deeper the water gets the
+        # less ability you have to pick up features on the seafloor and also
+        # features become less important the deeper the water gets as under
+        # keel clearance for ships becomes less of an issue.
+        failed_resolution = fds < feature_size
 
         # count of all cells/nodes/pixels that are not NaN in the uncertainty
         # array
@@ -641,6 +640,12 @@ class ResolutionCheck(GridCheck):
             tile.min_x,
             tile.min_y
         )
+
+        # Switch from a masked array to an array with a nodata value, this allows
+        # us to include nodata in the gdal output.
+        # fds.fill_value = -9999.0
+        # fds = fds.filled()
+
         # tf = '/Users/lachlan/work/projects/qa4mb/repo/mbes-grid-checks/au2.tif'
         # tile_ds = gdal.GetDriverByName('GTiff').Create(
         #     tf,
@@ -649,21 +654,22 @@ class ResolutionCheck(GridCheck):
         #     1,
         #     gdal.GDT_Float32
         # )
-        tile_ds = gdal.GetDriverByName('MEM').Create(
-            '',
-            tile.max_x - tile.min_x,
-            tile.max_y - tile.min_y,
-            1,
-            gdal.GDT_Float32
-        )
-        tile_ds.SetGeoTransform(tile_affine.to_gdal())
+        # tild_ds was only ever used to support debugging
+        # tile_ds = gdal.GetDriverByName('MEM').Create(
+        #     '',
+        #     tile.max_x - tile.min_x,
+        #     tile.max_y - tile.min_y,
+        #     1,
+        #     gdal.GDT_Float32
+        # )
+        # tile_ds.SetGeoTransform(tile_affine.to_gdal())
 
-        tile_band = tile_ds.GetRasterBand(1)
-        tile_band.WriteArray(fds, 0, 0)
-        tile_band.SetNoDataValue(-9999.0)
-        tile_band.FlushCache()
-        tile_ds.SetProjection(ifd.projection)
-        #
+        # tile_band = tile_ds.GetRasterBand(1)
+        # tile_band.WriteArray(fds, 0, 0)
+        # tile_band.SetNoDataValue(-9999.0)
+        # tile_band.FlushCache()
+        # tile_ds.SetProjection(ifd.projection)
+
         # tf2 = '/Users/lachlan/work/projects/qa4mb/repo/mbes-grid-checks/fu.tif'
         # tile_failed_ds = gdal.GetDriverByName('GTiff').Create(
         #     tf2,

tests/ausseabed/mbesgc/lib/test_gridcheck_resolution.py
@@ -26,26 +26,26 @@ class TestDensityCheck(unittest.TestCase):
 
         # set up some dummy data
         mask = [
-            [False,  False,  False, False],
-            [False,  False,  False, False],
-            [False,  False,  False, False],
-            [False,  False,  False, True],
-            [False,  False,  True, True]
+            [False, False, False, False],
+            [False, False, False, False],
+            [False, False, False, False],
+            [False, False, False, True],
+            [False, False, True, True]
         ]
         depth_data = [
-            [-40,  -40,  -40, -40],
-            [-40,  -60,  -80, -40],
-            [-40,  -60,  -70, -40],
-            [-40,  -30,  -70, -40],
-            [-40,  -40,  -40, -40]
+            [-40, -40, -40, -40],
+            [-40, -60, -80, -40],
+            [-40, -60, -70, -40],
+            [-40, -30, -70, -40],
+            [-40, -40, -40, -40]
         ]
         cls.depth = np.ma.array(
             np.array(depth_data, dtype=np.float32),
             mask=mask
         )
         density_data = [
-            [10,  1,  9, 9],
-            [10,  2, 10, 10],
+            [10, 1, 9, 9],
+            [10, 2, 10, 10],
             [10, 10, 10, 10],
             [10, 10, 10, 10],
             [10, 10, 10, 10],
@@ -55,11 +55,11 @@ class TestDensityCheck(unittest.TestCase):
             mask=mask
         )
         uncertainty_data = [
-            [0.7,  0.7,  0.2, 0.2],
-            [0.7,  0.4,  0.2, 0.2],
-            [0.2,  0.2,  0.2, 0.9],
-            [0.2,  0.2,  0.9, 0.0],
-            [0.2,  0.2,  0.2, 0.0]
+            [0.7, 0.7, 0.2, 0.2],
+            [0.7, 0.4, 0.2, 0.2],
+            [0.2, 0.2, 0.2, 0.9],
+            [0.2, 0.2, 0.9, 0.0],
+            [0.2, 0.2, 0.2, 0.0]
         ]
         cls.uncertainty = np.ma.array(
             np.array(uncertainty_data, dtype=np.float32),
@@ -105,7 +105,8 @@ class TestDensityCheck(unittest.TestCase):
         # the grid resolution by this value. So for the purposes of this test
         # we use a "Feature Detection Size Multiplier" value of 1.5 to get
         # some nodes to fail; in this case it means any nodes with a calculated
-        # feature detection size above 1.333 will fail this check.
+        # feature detection size below 1.333 will fail this check.
 
-        # and these values exceed threshold in 6 locations
-        self.assertEqual(check.failed_cell_count, 6)
+        # and these values exceed threshold in 11 locations. Note that it's not
+        # 14 locations due to the masking of 3 cells (lower right corner)
+        self.assertEqual(check.failed_cell_count, 11)