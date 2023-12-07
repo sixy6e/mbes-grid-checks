267f85b7938607219d17bedbdd28af27c696fc0d
Lachlan Hurst <lachlan.hurst@gmail.com>
2023-12-07 12:11:21 +1100

    removed redundant code now that grouping is handled by user in reworked UI

--- tests/ausseabed/mbesgc/qax/test_plugin.py
+++ tests/ausseabed/mbesgc/qax/test_plugin.py
@@ -1,31 +1,9 @@
 import unittest
 from typing import List
 
-from ausseabed.mbesgc.qax.plugin import FileGrouping, MbesGridChecksQaxPlugin
+from ausseabed.mbesgc.qax.plugin import MbesGridChecksQaxPlugin
 
 class TestGrouping(unittest.TestCase):
-
-    def test_calculate_grouping(self):
-        filename_list: List[str] = [
-            ("/test/foo/bar/in2018_c01_CombinedSurface_CUBE_2m_Rev2_depth.tif", "Survey DTMs"),
-            ("/test/foo/bar/in2018_c01_CombinedSurface_CUBE_2m_Rev2_density.tif", "Survey DTMs"),
-            ("/test/foo/bar/in2018_c01_CombinedSurface_CUBE_2m_Rev2_uncertainty.tif", "Survey DTMs"),
-            ("/test/foo/in2018_c02_CombinedSurface_CUBE_2m_Rev2_depth.tif", "Survey DTMs"),
-            ("/test/foo/in2018_c02_CombinedSurface_CUBE_2m_Rev2_density.tif", "Survey DTMs"),
-            ("/test/foo/in2018_c02_CombinedSurface_CUBE_2m_Rev2_uncertainty.tif", "Survey DTMs"),
-            ("/test/foo/in2018_c03_CombinedSurface_CUBE_2m_Rev2.tif", "Survey DTMs"),
-            ("/test/pinkcharts/in2018_c02_CombinedSurface_CUBE_2m_Rev2.shp", "Coverage Area"),
-            ("/test/pinkcharts/in2018_c01_CombinedSurface_CUBE_2m_Rev2.shp", "Coverage Area"),
-            ("/test/pinkcharts/in2018_c03_CombinedSurface_CUBE_2m_Rev2.shp", "Coverage Area")
-        ]
-
-        groups = FileGrouping.calculate_groupings(filename_list)
-
-        for g in groups:
-            print()
-            print(g.grouping_name)
-            for f,t in g.files:
-                print("    " + f)
 
     def test_extract_revision(self):
         fn = "foo_bar_r123_xyz"
